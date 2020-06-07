5179de682fb24c0be6e5e8711400905f7b180fe3
Karl Erik <karl0erik@gmail.com>
2020-06-07 21:46:33 +0200

    Add other comment

--- src/cljs/nr/about.cljs
+++ src/cljs/nr/about.cljs
@@ -18,13 +18,16 @@
            [:p "This is a lightly modified version of " [:a {:href "http://jinteki.net" :target "_blank"} "Jinteki.net"]
             "a website founded by " [:a {:href "http://twitter.com/mtgred" :target "_blank"} "@mtgred"]
             ", an avid Netrunner player from Belgium. For up-to-date information about contributors etc., please see the original site. "
-            "All credits, rights et cetera to the source code are entirely due to the original creators and contributors to Jinteki.net. "
-            "I'd also like to thank NoahTheDuke for being incredibly knowledgable and helpful in general, and for answering all kinds of "
-            "questions I had about Jinteki internals for this project in particular."]
+            "All credits, rights et cetera to the source code are entirely due to the original creators and contributors to Jinteki.net. "]
            [:p "The purpose of this page is to facilitate playtesting of an (unofficial) version of Netrunner, where the early cards in the game "
             "have been rebalanced. Changes have been kept minor where possible."
             " For discussion of the format, see #netrunner-reboot-project on Stimslack, and for a list of all card changes, see "
             [:a {:href "https://docs.google.com/spreadsheets/d/19GaP9AwNpvd-gC4sKNo7ZaxRYljb485Uno5VHf9p8As/edit#gid=1391682776" :target "_blank"} "this spreadsheet."]]
+
+           [:h3 "Acknowledgements"]
+           [:p "Thanks to Skippan made the image templates used for card generation."]
+           [:p "Thanks to NoahTheDuke for being knowledgable and helpful in general, and for answering all kinds of "
+            "questions I had about Jinteki internals for this project in particular."]
 
            [:h3 "Disclaimer"]
            [:p "Netrunner and Android are trademarks of Fantasy Flight Publishing, Inc. and/or Wizards of the Coast LLC."]
