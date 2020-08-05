38c51bdbad7b8b6de61a2b08947545c09e53edeb
Karl Erik <user.name>
2020-08-05 13:29:18 +0200

    Add first (still nonfunctional) pass of card gen

--- src/clj/game/deckgen.clj
+++ src/clj/game/deckgen.clj
@@ -12,6 +12,55 @@
 (def deck-stubs (atom {}))
 
 
+(defn gen-template
+  "Given a template, randomly choose an ID, then fill it with cards. Remove stuff from the template as you go."
+  ([template]
+   (gen-template (dissoc :identity template) {:identity (get @all-cards (rand-nth (:identity template)))
+                                              :cards []}))
+  ([template partial-deck]
+   (let [deck-id               (:identity partial-deck)
+         target-decksize       (+ (:minimum-deck-size deck-id) (if (= :corp (:side deck-id))
+                                                                  4 0))
+         target-ap              (+ 2 (* 2 (quot target-decksize 5)))
+         inf-limit             (:influence-limit deck-id)
+         current-ap            (reduce + (filter some? (map :agenda-points (:cards partial-deck))))
+         spent-inf             (reduce + (filter some? (map :influence-cost (:cards partial-deck))))
+         remaining-inf         (- inf-limit spent-inf)
+         remaining-ap          (- target-ap current-ap)]
+     (if (<= 0 (- target-decksize (count (:cards partial-deck))))
+       ;; if we're done, then we're done
+       partial-deck
+       
+       ;; choose a new random card matching one of the tags in our deck which doesn't take us over inf and keep going
+       (let [admissible-card (if (and (= :corp (:side deck-id))
+                                      remaining-ap)
+                               ;; ensure we fill up agenda points first, so only look at agendas we can play which aren't 0 pts or takes us over 20
+                               (fn [card] (and (= :agenda (:type card))
+                                              (#{:neutral-corp (:faction deck-id)} (:faction card))
+                                              (:agenda-points card)
+                                              (<= (:agenda-points card) remaining-ap)))
+                               
+                               ;; we're free to do whatever we want now, so choose a random tag in the template and go nuts
+                               (let [desired-tags (filter #(< 0 (% (:tags template)))
+                                                          (keys (:tags template)))
+                                     target-tag (when desired-tags (rand-nth desired-tags))] ;TODO: ensure this isn't weird - does rand-nth work on sets?
+                                 ;; TODO: write logic making a predicate which is true iff the card has that tag
+                                 nil))
+             chosen-card     (rand-nth (filter (fn [card] (and (admissible-card card)
+                                                              (or (= (:faction card) (:faction deck-id))
+                                                                  (and (:influence-cost card)
+                                                                       (<= (:influence-cost card) remaining-inf)))))
+                                               @all-cards))
+             ;; TODO: add logic making a set of the tags of a card here
+             card-tags       nil]
+
+         ;; having chosen a random card, add it to the deck, decrement all the tags it satisfies in the template by 1 and move on
+         (let [new-deck     (conj (:cards partial-deck))
+               new-template (update template :tags (fn [tagmap]
+                                                     (reduce-kv (fn [coll tag num] (if (card-tags tag) (dec num) num))
+                                                                {} tagmap)))])
+         (gen-template new-template new-deck))))))
+
 
 (defn parse-stub
   [stub]
