e8786af13ada67bfedd21e7982a169f51745cf0c
Karl Erik <karlerik@clover>
2022-08-27 15:07:55 +0200

    Add donators listing and some more up-to-date info

--- src/cljs/nr/about.cljs
+++ src/cljs/nr/about.cljs
@@ -21,17 +21,30 @@
             "All credits, rights et cetera to the source code are entirely due to the original creators and contributors to Jinteki.net. "]
            [:p "The purpose of this page is to facilitate playtesting of an (unofficial) version of Netrunner, where the early cards in the game "
             "have been rebalanced. Changes have been kept minor where possible."
-            " For discussion of the format, see #netrunner-reboot-project on Stimslack, and for a list of all card changes, see "
-            [:a {:href "https://docs.google.com/spreadsheets/d/19GaP9AwNpvd-gC4sKNo7ZaxRYljb485Uno5VHf9p8As/edit#gid=1391682776" :target "_blank"} "this spreadsheet."]]
+            " For discussion of the format, visit "
+            [:a {:href " https://discord.gg/YZPgyJwVKFsee" :target "_blank"} "this Discord,"]
+            "and for an overview of all changes, see"
+            [:a {:href "https://sites.google.com/view/netrunner-reboot-project/" :target "_blank"} "here."]]
 
            [:h3 "Acknowledgements"]
-           [:p "Thanks to Skippan made the image templates used for card generation."]
+           [:p "Thanks to Skippan, who significantly improved the image templates used for card generation."]
            [:p "Thanks to NoahTheDuke for being knowledgable and helpful in general, and for answering all kinds of "
             "questions I had about Jinteki internals for this project in particular."]
 
+           [:h3 "Donations"]
+           [:p "Although nobody makes any profit from running this page, donators help keep the server "
+            "running. If you are interested in helping out, you will get access to full-art versions of the cards as a token of gratitude - see "
+            [:a {:href "https://ko-fi.com/reboot_dev" :target "_blank"} "here"] " for details."]
+
+           [:p "Many thanks to all donors!"]
+           [:ul.list.compact
+            (for [d @donors]
+              ^{:key d}
+              [:li d])]
+
            [:h3 "Disclaimer"]
            [:p "Netrunner and Android are trademarks of Fantasy Flight Publishing, Inc. and/or Wizards of the Coast LLC."]
-           [:p "This is website is not affiliated with Fantasy Flight Games or Wizards of the Coast."]
+           [:p "This is website is not affiliated with Fantasy Flight Games, Wizards of the Coast or NISEI, and does not profit off any of their intellectual properties."]
            [:p "Targeting icon made by "
             [:a {:href "http://www.freepik.com" :title "Freepik" :target "_blank"} "Freepik"]
             " from "
